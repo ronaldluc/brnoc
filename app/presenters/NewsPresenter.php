ae106e2066220931436ee9184e9f49061b8916ca
norik16 <Rahaga16>
2015-12-24 01:34:08 +0100

    Initial commit

---
+++ app/presenters/NewsPresenter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: norik
+ * Date: 23.12.15
+ * Time: 00:48
+ */
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class NewsPresenter extends Nette\Application\UI\Presenter
+{
+}

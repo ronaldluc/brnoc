213d250a51fee8433c4cd72f18e544609cb4c6b7
norik16 <Rahaga16>
2015-12-25 02:57:21 +0100

    Added Registered page

--- app/presenters/NewsPresenter.php
+++ app/presenters/NewsPresenter.php
@@ -11,6 +11,8 @@ namespace App\Presenters;
 use Nette;
 
 
+
 class NewsPresenter extends Nette\Application\UI\Presenter
 {
+
 }
---
+++ app/presenters/RegisteredPresenter.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: norik
+ * Date: 25.12.15
+ * Time: 2:22
+ */
+
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+
+
+class RegisteredPresenter extends Nette\Application\UI\Presenter
+{
+	/** @var  @var Nette\Databe\Context */
+	private $database;
+
+	public function __construct(Nette\Database\Context $database)
+	{
+		$this->database = $database;
+	}
+
+	public function renderDefault()
+	{
+		$this->template->users = $this->database->table('user')
+			->order('created DESC')
+			->limit(5);
+	}
+}
